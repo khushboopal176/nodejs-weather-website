ae71e8de2ee8d6b46f6807f28dc792fbcde34192
Khushboo <khushboopal176@gmail.com>
2020-05-26 20:39:52 +0530

    setup app for heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast= (latitude , longitude, callback )=>{
         } else if(body.error){
             callback('Unable to find location', undefined)
         } else{
-            callback(undefined,  body.daily.data[0].summary +' It is currently ' + body.currently.temperature +' degrees out. The temperature high for the day is ' + body.daily.data[0].temperatureHigh + 'degrees. The temperature low for the day is ' + body.daily.data[0].temperatureLow +'degress. There is a '+  body.currently.precipProbability + ' % chance of rain.')
+            callback(undefined,  body.daily.data[0].summary +' It is currently ' + body.currently.temperature +' degrees out. The temperature high for the day is ' + body.daily.data[0].temperatureHigh + ' degrees. The temperature low for the day is ' + body.daily.data[0].temperatureLow +' degress. There is a '+  body.currently.precipProbability + ' % chance of rain.')
         }
 
     }) 
